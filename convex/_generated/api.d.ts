2178f22d2dc32befe3fe6f78c34362eed263249e
Anjana Vakil <anjanavakil@gmail.com>
2023-07-20 11:00:29 -0700

    Add init.ts to seed data

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as init from "../init";
 import type * as messages from "../messages";
 
 /**
@@ -25,6 +26,7 @@ import type * as messages from "../messages";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  init: typeof init;
   messages: typeof messages;
 }>;
 export declare const api: FilterApi<
