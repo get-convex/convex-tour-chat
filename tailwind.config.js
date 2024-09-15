e061a400f94bcc54d1a308e967672e77e330d487
Apurva <92764056+avarigonda24@users.noreply.github.com>
2024-09-15 05:27:23 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+          // Define your custom colors here
+          primary: {
+            light: '#c8d9ed',
+            DEFAULT: '#4663ac',
+            dark: '#314152',
+          },
+          secondary: {
+            light: '#e1f1fd',
+            DEFAULT: '#d2deeb',
+            dark: '#c1d8f0',
+          },
+          customGreen: '#32CD32', // Single color without shades
+        },
+    },
   },
   plugins: [],
 }
