290304979a55744a6b6459aa3c11e3d23ed45d62
Sujay Jayakar <sujayakar@convex.dev>
2025-03-04 14:45:52 -0500

    Release 1.19.5
    834173eddeb7564effac7ae2f6fd540997530915

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as ai from "../ai.js";
 import type * as init from "../init.js";
 import type * as messages from "../messages.js";
 
@@ -25,6 +26,7 @@ import type * as messages from "../messages.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  ai: typeof ai;
   init: typeof init;
   messages: typeof messages;
 }>;
