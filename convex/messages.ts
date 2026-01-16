4a90750527c058d95cbf340feb1013c0f587a066
Reece Yang <7040416+reeceyang@users.noreply.github.com>
2026-01-15 16:22:34 -0800

    Release 1.31.5
    64fd1aaff3d101da664c51f30343f44d0d195c26

--- convex/messages.ts
+++ convex/messages.ts
@@ -6,8 +6,22 @@ export const list = query({
   handler: async (ctx) => {
     // Grab the most recent messages.
     const messages = await ctx.db.query("messages").order("desc").take(100);
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        // Find the likes for each message
+        const likes = await ctx.db
+          .query("likes")
+          .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+          .collect();
+        // Join the count of likes with the message data
+        return {
+          ...message,
+          likes: likes.length,
+        };
+      }),
+    );
     // Reverse the list so that it's in a chronological order.
-    return messages.reverse().map((message) => ({
+    return messagesWithLikes.reverse().map((message) => ({
       ...message,
       // Format smileys
       body: message.body.replaceAll(":)", "😊"),
@@ -17,8 +31,19 @@ export const list = query({
 
 export const send = mutation({
   args: { body: v.string(), author: v.string() },
-  handler: async (ctx, { body, author }) => {
+  handler: async (ctx, args) => {
     // Send a new message.
-    await ctx.db.insert("messages", { body, author });
+    await ctx.db.insert("messages", { body: args.body, author: args.author });
+  },
+});
+
+export const like = mutation({
+  args: { liker: v.string(), messageId: v.id("messages") },
+  handler: async (ctx, args) => {
+    // Save a user's "like" of a particular message
+    await ctx.db.insert("likes", {
+      liker: args.liker,
+      messageId: args.messageId,
+    });
   },
 });
